2f57d17ee0386e899080685da39be343e40a0fcd
Ryan Coppolo <r@coppolo.com>
2012-03-16 19:01:23 -0700

    Rocks some authentification.

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+	attr_accessible :email, :password, :password_confirmation
+	has_secure_password
+	validates :password, presence: true, on: :create
+	validates :email, presence: true, on: :create
+end
