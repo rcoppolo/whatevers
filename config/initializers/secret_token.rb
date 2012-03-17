62a8c928fcedcbed635888ad55c2b047662e8aa0
Ryan Coppolo <r@coppolo.com>
2012-03-16 18:10:19 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whatevers::Application.config.secret_token = 'cd916785bb8cc768654be8a965e04d80bd87b24a21cd284951b1822c4555151ba1428b9600c7040da05803e4e490d87e4ad82e5933bf7dd6aef7a34351b067db'
